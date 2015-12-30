d06325283065f1e51f74e312a33e0dd5a3f28cdd
Patrick Arlt <patrick.arlt@gmail.com>
2015-12-30 09:00:30 -0800

    build 1.0.0

---
+++ dist/binary-search-index.js
@@ -0,0 +1,94 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.BinarySearchIndex = factory();
+}(this, function () { 'use strict';
+
+  class BinarySearchIndex {
+    constructor (values = []) {
+      this.values = [].concat(values);
+    }
+
+    query (value) {
+      var index = this.getIndex(value);
+      return this.values[index];
+    }
+
+    getIndex (value) {
+      if (this.dirty) {
+        this.sort();
+      }
+
+      var minIndex = 0;
+      var maxIndex = this.values.length - 1;
+      var currentIndex;
+      var currentElement;
+
+      while (minIndex <= maxIndex) {
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        currentElement = this.values[Math.round(currentIndex)];
+        if (+currentElement.value < +value) {
+          minIndex = currentIndex + 1;
+        } else if (+currentElement.value > +value) {
+          maxIndex = currentIndex - 1;
+        } else {
+          return currentIndex;
+        }
+      }
+
+      return Math.abs(~maxIndex);
+    }
+
+    between (start, end) {
+      var startIndex = this.getIndex(start);
+      var endIndex = this.getIndex(end);
+
+      if (startIndex === 0 && endIndex === 0) {
+        return [];
+      }
+
+      while (this.values[startIndex - 1] && this.values[startIndex - 1].value === start) {
+        startIndex--;
+      }
+
+      while (this.values[endIndex + 1] && this.values[endIndex + 1].value === end) {
+        endIndex++;
+      }
+
+      if (this.values[endIndex].value === end) {
+        endIndex++;
+      }
+
+      return this.values.slice(startIndex, endIndex);
+    }
+
+    insert (item) {
+      this.values.splice(this.getIndex(item.value), 0, item);
+      return this;
+    }
+
+    bulkAdd (items = [], sort = false) {
+      this.values = this.values.concat([].concat(items));
+
+      if (sort) {
+        this.sort();
+      } else {
+        this.dirty = true;
+      }
+
+      return this;
+    }
+
+    sort () {
+      this.values.sort(function (a, b) {
+        return +b.value - +a.value;
+      }).reverse();
+      this.dirty = false;
+      return this;
+    }
+  }
+
+  return BinarySearchIndex;
+
+}));
+//# sourceMappingURL=binary-search-index.js.map
